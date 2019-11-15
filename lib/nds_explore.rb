84ed80f39cbd4a69a2230784ac11deeb6b37a6e1
Ajani Queeley <ajaniqueeley@gmail.com>
2019-11-15 07:38:50 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,4 +10,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  movie = directors_database[0][:movies]
+  index = 0
+
+  while index < movie.length do
+    titles = movie[index][:title]
+    puts titles
+    index +=1
+  end
 end
